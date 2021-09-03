6436d89f8204c37a55ec4441223a1bcb637c7aa5
Daniel Karlsson <led.karlsson@gmail.com>
2021-09-03 23:00:36 +0200

    Move init into Game

--- modified/TowerDefense.py
+++ modified/TowerDefense.py
@@ -32,35 +32,28 @@ displayTower = None
 
 class Game():
      def __init__(self):
-          pass
-
-class TowerDefenseGame(Game):
-     def __init__(self): #setting up the window for the game here
           self.root=Tk() #saying this window will use tkinter
           self.root.title("Tower Defense Ultra Mode")
           self.running=True #creating a variable RUN. does nothing yet.hu
           self.root.protocol("WM_DELETE_WINDOW", self.end)
-
           self.frame = Frame(master= self.root)
           self.frame.grid(row = 0, column = 0)
 
           self.canvas = Canvas(master = self.frame, width=mapSize, height=mapSize, bg = "white", highlightthickness = 0) #actually creates a window and puts our frame on it
           self.canvas.grid(row = 0,column = 0,rowspan = 2, columnspan = 1) #makes the window called "canvas" complete
 
+
+class TowerDefenseGame(Game):
+     def __init__(self): #setting up the window for the game here
+          super().__init__()
+
           self.displayboard = Displayboard(self)
-          
           self.infoboard = Infoboard(self)
-
           self.towerbox = Towerbox(self)
-
           self.mouse = Mouse(self)
-
           self.gameMap = Map()
-
           self.wavegenerator = Wavegenerator(self)
-
           self.run() #calls the function 'def run(self):'
-
           self.root.mainloop() #starts running the tkinter graphics loop
 
      def run(self):
